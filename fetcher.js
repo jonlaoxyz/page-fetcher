abf7d979ec08897523f99c08ad078d80d09e1ce6
Jonathan Lao <jon.lao@gmail.com>
2023-11-23 00:02:56 -0600

    added comments in fetcher.js

--- fetcher.js
+++ fetcher.js
@@ -1,9 +1,13 @@
 const request = require('request');
 const fs = require("fs");
 
+// assign process.argv selected values to path and domain variables;
 const path = process.argv[3];
 const domain = process.argv[2];
 
+// run the request command and assign the error, response, body
+// - based off the console input for domain and write the output using fs to
+// a file "path" - based from 2nd input from console.
 request(domain, (error, response, body) => {
   if (error) {
     console.log('error:', error);
